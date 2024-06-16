1a1ad248bbef118d0dfc31012a6c1f0fb12947d6
cmpsodusta <cmpsodusta.chmsu@gmail.com>
2024-06-17 00:40:22 +0800

    Update home.py

--- home.py
+++ home.py
@@ -9,7 +9,7 @@ show_pages(
         Section("Applications"),
         Page("pages/crop_recom_streamlitapp.py", "Prediction", "1️⃣", in_section=True),
         Page("pages/basic_sentiment_analyzer.py", "Sentiment Analysis", "2️⃣", in_section=True),
-        Page("pages/activity_endterm_4.py", "Image Classification", "3️⃣", in_section=True),
+        Page("pages/image_classification.py", "Image Classification", "3️⃣", in_section=True),
 ]
 )
 
